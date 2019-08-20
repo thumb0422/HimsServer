6cdc75995e2e0ff05eb511a32b09fcd3e88e9671
liuchunhua1 <chunhua1@eastmoney.com>
2019-08-20 09:53:18 +0800

    blueprint

--- api/models.py
+++ api/models.py
@@ -10,7 +10,7 @@ from api import db,app
 
 class Todo(db.Model):
     __tablename__ = 'todos'
-    __table_args__ = {"useexisting": True}
+    __table_args__ = {"extend_existing": True}
     id = db.Column('todo_id', db.Integer, primary_key=True)
     title = db.Column(db.String(60))
     text = db.Column(db.String)
